b0e286f8434a525df67293ffdfb9c65c14cc454b
Eduard Valentin <eduard.valentin96@yahoo.it>
2017-12-18 15:31:31 +0200

    Update SubstringPD.cpp

--- Advanced Proggramming Techinques/Lab3 - Dynamic Programming/SubstringPD.cpp
+++ Advanced Proggramming Techinques/Lab3 - Dynamic Programming/SubstringPD.cpp
@@ -1,5 +1,5 @@
 
-/* 
+/*
 Subproblema		 ==> l[i] = lungimea maxima a unui subsir ce contine cuvantul i
 					 poz[c1 c2] = pozitia la care se gaseste cuvantul ce contine ca prime doua litere c1 si c2 si formeaza un subsir maximal cu elementele deja procesate
 					 p[i] = parintele cuvantului i
@@ -10,12 +10,11 @@ Solutie			 ==> max{l[i] | i = 1,n}
 Regula de calcul ==> i = n,1
 */
 
-
-#include "stdafx.h"
 #include <iostream>
 #include <string>
 #include <map>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
@@ -30,8 +29,9 @@ void afisDrum(vector<string> &v, vector<int> &p, int idx)
 
 int main()
 {
+    ifstream fin("date.in");
 	size_t n;
-	cin >> n;
+	fin >> n;
 	vector<string> v(n);
 	vector<int> p(n, -1);
 	vector<int> l(n, 1);
@@ -40,32 +40,34 @@ int main()
 
 	for (size_t i = 0; i < n; i++)
 	{
-		cin >> v[i];
+		fin >> v[i];
 	}
 
 	for (int i = n-1; i >= 0; i--)
 	{
+	    // iau primele doua litere din cuvantul curent
 		string firstTwo;
 		firstTwo += v[i][0];
 		firstTwo += v[i][1];
 
+        // iau ultimele doua litere din cuvantul curent
 		string lastTwo;
 		lastTwo += v[i][v[i].length() - 2];
 		lastTwo += v[i][v[i].length() - 1];
 
-		map<string, int>::iterator it_ftwo = poz.find(firstTwo);
-		map<string, int>::iterator it_ltwo = poz.find(lastTwo);	
+		map<string, int>::iterator it_ftwo = poz.find(firstTwo);    // pozitia la care gasesc un cuvant ce are ca prime doua litere firstTwo (si care formeaza un
+		map<string, int>::iterator it_ltwo = poz.find(lastTwo);     // pozitia la care gasesc un cuvant ce are ca prime doua litere lastTwo
 
 		if (it_ltwo != poz.end())
 		{
-			// Am calculat inainte
+			// Am calculat inainte si pot sa lipesc la un cuvant (optim)
 			l[i] = l[it_ltwo->second] + 1;
 			p[i] = it_ltwo->second;	// Legam cu parintele
 		}
 
 		if (it_ftwo != poz.end())
 		{
-			if (l[it_ftwo->second] < l[i])	// Actualizam poz daca e cazul
+			if (l[it_ftwo->second] < l[i])	// Actualizam poz daca subsirul format la pasul curent are o lungime mai mare decat subsirul procesat anterior
 				it_ftwo->second = i;
 		}
 		else
@@ -85,4 +87,3 @@ int main()
 	afisDrum(v, p, k);
 	return 0;
 }
-
